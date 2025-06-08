a60ff9160fd75accbe9d44829f2871529b61094b
ilyasgy <ilyasingy@Gmail.com>
2025-06-09 01:43:46 +0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/json", function(req, res) {
+  res.json({ message: "Hello json" });
+});
+
 // app.get("/", function(req, res) {
 //   res.send("Hello Express");
 // });
