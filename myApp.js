d22451f3b4398bc3e3acdd76cc69a06465cf3aba
ilyasgy <ilyasingy@Gmail.com>
2025-06-10 01:10:06 +0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -4,6 +4,11 @@ var app = express();
 
 app.use("/public", express.static(__dirname + "/public"));
 
+app.use(function(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
 app.get("/json", function(req, res) {
   const msg = process.env.MESSAGE_STYLE === "uppercase"
     ? "HELLO JSON"
